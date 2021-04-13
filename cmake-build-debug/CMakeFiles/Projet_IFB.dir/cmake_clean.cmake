e36f89cd1d25d1e654abc73b2bf3b57057bc0a39
Placard <31778398+estebanbecker@users.noreply.github.com>
2021-04-13 17:54:16 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Projet_IFB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_IFB.dir/main.c.obj"
+  "Projet_IFB.exe"
+  "Projet_IFB.exe.manifest"
+  "Projet_IFB.pdb"
+  "libProjet_IFB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_IFB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

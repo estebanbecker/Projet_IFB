0b4764d120b607b90dd95ddd6292c1799823a06b
Monsieur_Placard <31778398+estebanbecker@users.noreply.github.com>
2021-05-18 19:35:56 +0200

    Implementation grille dans addtoken.c

--- addtoken.h
+++ addtoken.h
@@ -4,5 +4,9 @@
 
 #ifndef PROJET_IFB_ADDTOKEN_H
 #define PROJET_IFB_ADDTOKEN_H
+#include "table.h"
+
+int addtoken(int jeton, int input, grid *pgrid);
+int removetoken(int jeton, int input, grid *pgrid);
 
 #endif //PROJET_IFB_ADDTOKEN_H

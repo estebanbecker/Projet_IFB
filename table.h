0c8c732b8beb2598cfe9a9a0c9f9cdb73fa16c08
Monsieur_Placard <31778398+estebanbecker@users.noreply.github.com>
2021-05-25 13:09:06 +0200

    Ajout commentaire dans table.h

--- table.h
+++ table.h
@@ -10,8 +10,18 @@ typedef struct{
     char **data;
 }grid;
 
+/**
+ * Créer une grille en deux dimension
+ * @param side Nombre de case par coté de la grille
+ * @param table Pointeur de la structure grid a modifier
+ */
 
 void new_grid(int side,grid *table);
+
+/**
+ * Affiche la grille
+ * @param table structure grille de la grille à afficher
+ */
 
 void show_grid(grid table);
 

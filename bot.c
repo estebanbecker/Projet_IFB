cf7b1db815a9bbebc3e1b65b94de468a159d6e71
Monsieur_Placard <31778398+estebanbecker@users.noreply.github.com>
2021-06-01 11:07:49 +0200

    Ajouts des contrôles des entrées utilisateurs

--- bot.c
+++ bot.c
@@ -10,19 +10,19 @@
 
 void bot(grid *table,int ban_col){
 
-    int buffer1,buffer2;//buffer1 pour ctocker l'endroit où jouer, buffer2 permet de savoir si le bot a joué
+    int buffer1,has_play;//buffer1 pour stocker l'endroit où jouer, has_play permet de savoir si le bot a joué
 
     do{
-        buffer2=0;//réinitialise le buffer
+        has_play=0;//réinitialise le buffer
 
         buffer1=rand()%(*table).side;//gener une colonne aléatoire
 
         if((rand()/5)<4){
 
-            if(valid_add_colonne(buffer1,*table)){//vérifie si l'action peut être effectué
+            if(valid_add_colonne(buffer1,*table) && buffer1!=ban_col){//vérifie si l'action peut être effectué
                 addtoken('X',buffer1,table);
             }else{
-                buffer2=1; //empêche le programme de sortir de la boucle
+                has_play=1; //empêche le programme de sortir de la boucle
             }
 
         }else{
@@ -30,13 +30,13 @@ void bot(grid *table,int ban_col){
             if(valid_remove_colonne(buffer1,*table)){//vérifie si l'action peut être effectué
                 removetoken(buffer1,table);
             }else{
-                buffer2=1;//empêche le programme de sortir de la boucle
+                has_play=1;//empêche le programme de sortir de la boucle
             }
 
         }
 
 
-    } while (buffer2);//vérifie que l'action a été effectué
+    } while (has_play);//vérifie que l'action a été effectué
 
     srand(time(0));
 
--- main.c
+++ main.c
@@ -7,35 +7,50 @@
 int main() {
 
     grid table;
-    grid *ptable;
-    int buffer=-1;
-    int *pbuffer;
+    int colonne_ban=-1;//Colonne interdite suite au retrait d'un jeton (-1 si aucune)
     int type_part;
-    char turn='X';
+    char turn='X';//Stock le jeton du joueur en cours
     int action;
-    int input;
+    int colonne_input;//Colonne entrée
     int quit;
-    int i;
-    int checkup=0;
-    char name[30];
-    pbuffer=&buffer;
-    ptable=&table;
-    int size;
-    printf("----- PUISSANCE N ----");
+    int ligne;//Ligne où le point a été ajouté
+    int checkup;
+    char name[30];//Nom du fichier de sauvegarde
+    int size;//Taille de la grille
+
+
+    printf("----- PUISSANCE N ----\n");
     printf("(1) Nouvelle partie? - ");
-    printf("(2) Charger partie");
-    scanf("%d", &action);
+    printf("(2) Charger partie\n");
+
+    scanf("%d", &action);//Entrée du mode avec message d'erreur
+    fflush(stdin);//Nettoyage de l'entrée au cas où l'utilisateur entre une lettre
+    while (action<1 || 2<action){
+        printf("L'action n'existe pas, veuillez saisir une valeur entre 1 et 2: ");
+        scanf("%d", &action);
+        fflush(stdin);
+    }
+
     if(action==1){
     printf("Quelle taille puissance N\n");
     scanf("%d", &size);
+    fflush(stdin);
+        while (action<=0){
+            printf("Veuillez entrer une valeur superieur a 0: ");
+            scanf("%d", &action);
+            fflush(stdin);
+        }
+
     new_grid(size+2,&table);
     }
     else{
         if(action==2){
-            printf("Nom de sauvegarde??");
-            fflush(stdin);
-            gets(name);
-            load(&turn,&buffer,&type_part,&table,name);
+            do{
+                printf("Nom de sauvegarde:\n");
+                fflush(stdin);
+                gets(name);
+            }while(!load(&turn,&colonne_ban,&type_part,&table,name));
+            size=table.side-2;
         }
     }
 
@@ -44,36 +59,53 @@ int main() {
         show_grid(table);
         printf("(1) Jouer jeton? - ");
         printf("(2) Effacer jeton? - ");
-        printf("(3) Sauvegarder");
+        printf("(3) Sauvegarder\n");
+
         scanf("%d", &action);
+        fflush(stdin);
+        while (action<1 || 3<action){//Entrée du mode avec message d'erreur
+            printf("L'action n'existe pas, veuillez saisir une valeur entre 1 et 3: ");
+            scanf("%d", &action);
+            fflush(stdin);
+        }
+
         if(action==1){
             checkup=0;
             while(checkup!=1) {
-                printf("Où ajouter jeton?? 1 - %d", size+2);
-                scanf("%d", &input);
-                if (input - 1 != buffer) {
+                printf("Ou ajouter jeton? 1 - %d\n", size+2);
+
+                scanf("%d", &colonne_input);//Aquisition de l'emplacement du jeton avec message d'erreur
+                fflush(stdin);
+                while (0>=colonne_input || colonne_input > table.side){
+                    printf("La colonne n'existe pas \n");
+                    printf("Ou ajouter jeton? 1 - %d\n", size+2);
+                    scanf("%d", &colonne_input);
+                    fflush(stdin);
+                }
+
+                if (colonne_input - 1 != colonne_ban) {
                     checkup=1;
-                }else if(buffer==input-1){
+                }else if(colonne_ban==colonne_input-1){
                 printf("Vous ne pouvez pas jouer dans cette colonne.\n");
                 }
             }
-            buffer=-1;
-            i=addtoken(turn, input-1, &table);
+            colonne_ban=-1;
+            ligne=addtoken(turn, colonne_input-1, &table);
             show_grid(table);
-            checkwinner(turn, size, i, input-1, table);
+            checkwinner(turn, size, ligne, colonne_input-1, table);
         }else{
             if(action==2){
                 printf("Jeton à supprimer 1 - %d", size+2);
-                scanf("%d", &input);
-                buffer=input-1;
-                removetoken(input-1, &table);
+                scanf("%d", &colonne_input);
+                colonne_ban=colonne_input-1;
+                removetoken(colonne_input-1, &table);
                 show_grid(table);
             }else{
                 if(action==3){
-                    printf("Nom de sauvegarde???\n");
+                    printf("Nom de sauvegarde?\n");
                     fflush(stdin);
                     gets(name);
-                    save(turn, buffer, 1, table, name);
+                    save(turn, colonne_ban, 1, table, name);
                     printf("Quitter?  - 0/1\n");
                     scanf("%d", &quit);
                     if(quit==1){
